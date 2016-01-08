193a84609d6a18f66f908bfb1a67df501e720089
Andrey Sitsko <andrey.sitsko@gmail.com>
2016-01-09 02:46:15 +0300

    add signIn controller, config signIn view and service

--- app/share/components/signIn/signInApp.js
+++ app/share/components/signIn/signInApp.js
@@ -3,6 +3,13 @@ var app = angular.module('signInApp', []);
 app.directive('signInForm', function() {
   return {
     restrict: 'E',
-    templateUrl: 'signInView.html'
+    templateUrl: 'signInView.html',
+    controller: ['$scope','loginService', function($scope, loginService) {
+      $scope.password = '';
+      $scope.email = '';
+      $scope.submitSignIn = function() {
+        loginService.signIn({email: $scope.email, password: $scope.password});
+      };
+    }]
   };
 });
--- app/share/services/loginService.js
+++ app/share/services/loginService.js
@@ -3,7 +3,7 @@ var app = angular.module('loginServiceApp', []);
 app.service('loginService', ['$http', function($http) {
     this.signUp = function(authData) {
       $http.post('/signUp', authData).then(
-          function(success) {
+          function(res) {
               console.log('signUp success callback' + success);
           }, function(error) {
               console.log('signUp errr callback' + error);
@@ -11,6 +11,16 @@ app.service('loginService', ['$http', function($http) {
     };
 
     this.signIn = function(authData) {
-      $http.post('/signIn', authData);
+      $http.post('/signIn', authData).then(
+          function(res) {
+              if(res.data.success) {
+                  console.log('user found token:' + res.data.token);
+              } else {
+                  console.log('user not found');
+              }
+
+          }, function(error) {
+              console.log('signIn errr callback' + error);
+          });
     };
 }]);
