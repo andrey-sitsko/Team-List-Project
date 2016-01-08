2404f382eeafbc52180319fb53cc96378b28f75e
Andrey Sitsko <andrey.sitsko@gmail.com>
2016-01-09 02:25:58 +0300

    config signIn routing

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -1,4 +1,5 @@
-var User = require('../models/userModel.js');
+var User = require('../models/userModel.js'),
+    jwt = require('jsonwebtoken');
 
 function loginRoutes(app) {
 
@@ -12,11 +13,41 @@ function loginRoutes(app) {
       password: req.body.password,
       email: req.body.email
     });
-
     user.save(function(err) {
       if (err) throw err;
     });
+  });
 
+  app.post('/signIn', function(req, res) {
+    User.findOne({
+      email: req.body.email
+    },
+    function(err, user) {
+      if (err) {
+        throw err;
+      }
+      if (!user || req.body.password != user.password) {
+        res.json({ success: false, error: 'notFound' });
+      } else {
+        var token = jwt.sign(user, 'shhhhh', {
+          expiresIn: '1d'
+        });
+        res.json({
+          success: true,
+          token: token
+        });
+      }
+    });
+  });
+
+  app.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+      res.send(users);
+    });
+  });
+
+  app.get('/deleteAll', function(req, res) {
+    User.remove({}, function(err) {});
   });
 
   return app;
