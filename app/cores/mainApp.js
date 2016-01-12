82df4966e4e9c1dd42904e7290b80374c19048f2
Andrey Sitsko <andrey.sitsko@gmail.com>
2016-01-13 00:36:55 +0300

    add header to main view

--- app/cores/mainApp.js
+++ app/cores/mainApp.js
@@ -1,1 +1,11 @@
-var app = angular.module('mainApp',  []);
+require('../../node_modules/angular/angular.min.js');
+require('../share/components/header/headerApp.js');
+
+var app = angular.module('mainApp',  ['headerApp']);
+
+app.controller('styleController', ['$scope', function($scope) {
+  $scope.stylesheets = [
+    '../share/components/header/headerStyle.css',
+    '../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+  ];
+}]);
