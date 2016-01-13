652f615c0555922e23e18197364d54a40b48e144
Andrey Sitsko <andrey.sitsko@gmail.com>
2016-01-13 20:59:00 +0300

    move signOut logic to separate module

--- server/middleware/login/signOut.js
+++ server/middleware/login/signOut.js
@@ -1,1 +1,7 @@
 var User = require('../../models/userModel.js');
+
+module.exports = function(req, res) {
+  req.session.destroy();
+  req.logout();
+  res.redirect('/');
+};
--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -3,15 +3,16 @@ var User = require('../models/userModel.js'),
     LocalStrategy = require('passport-local').Strategy,
     signIn = require('../middleware/login/signIn'),
     signUp = require('../middleware/login/signUp'),
+    signOut = require('../middleware/login/signOut'),
     isAuthenticated = require('../middleware/login/isAuthenticated');
 
 function loginRoutes(app) {
 
-  app.get('/logout', function(req, res) {
-    req.session.destroy();
-    req.logout();
-    res.redirect('/');
-  });
+  app.get('/logout', signOut);
+
+  app.post('/signUp', signUp);
+
+  app.post('/signIn', signIn);
 
   app.get('/', function(req, res) {
     res.render('loginView.html');
@@ -20,10 +21,6 @@ function loginRoutes(app) {
   app.get('/main', isAuthenticated, function(req, res) {
     res.render('mainView.html');
   });
-
-  app.post('/signUp', signUp);
-
-  app.post('/signIn', signIn);
 
   app.get('/api/users', function(req, res) {
     User.find({}, function(err, users) {
