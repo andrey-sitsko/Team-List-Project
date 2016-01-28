21a3305d62bbf4873cc617151ba6a450ee64309b
Andrey Sitsko <andrey.sitsko@gmail.com>
2016-01-28 12:38:14 +0300

    move dom manipulation and login callback to login service

--- app/share/components/login/signIn/signInApp.js
+++ app/share/components/login/signIn/signInApp.js
@@ -1,32 +1,16 @@
 require('../../../services/loginService.js');
-require('../../../services/userStorageService.js');
 
-var app = angular.module('signInApp', ['loginServiceApp', 'userStorageServiceApp']);
+var app = angular.module('signInApp', ['loginServiceApp']);
 
 app.directive('signInForm', function() {
   return {
     restrict: 'E',
     templateUrl: 'signInView.html',
-    controller: ['$scope','$window', 'loginService', 'userStorageService', function($scope, $window, loginService, userStorageService) {
+    controller: ['$scope','$window', 'loginService', function($scope, $window, loginService) {
       $scope.signInPassword = '';
       $scope.signInEmail = '';
       $scope.submitSignIn = function() {
-        loginService.signIn({email: $scope.signInEmail, password: $scope.signInPassword}, function (res) {
-          if(res.data.success) {
-            userStorageService.set(res.data.user);
-            $window.location.href = '/main';
-          } else {
-            $scope.signInPassword = '';
-            $('#sign-in-email').addClass('wrong-login-credetians');
-            $('#sign-in-password').addClass('wrong-login-credetians');
-            $('#wrong-login-credetians-message').addClass('show-login-tooltip');
-            $('#wrong-login-credetians-message').on("animationend", function() {
-              $('#sign-in-email').removeClass('wrong-login-credetians');
-              $('#sign-in-password').removeClass('wrong-login-credetians');
-              $('#wrong-login-credetians-message').removeClass('show-login-tooltip');
-            });
-          }
-        });
+        loginService.signIn({email: $scope.signInEmail, password: $scope.signInPassword}, loginService.processLoginResults);
       };
     }]
   };
