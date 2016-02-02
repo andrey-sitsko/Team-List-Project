6ac87e057aecd589b006a39913b906dfa78867f1
Andrey Sitsko <andrey.sitsko@gmail.com>
2016-02-02 10:52:37 +0300

    config adding lists changes to local storage

--- app/share/components/main/lists/listsService.js
+++ app/share/components/main/lists/listsService.js
@@ -1,19 +1,21 @@
 require('../../../services/idGeneratorService.js');
+require('../../../services/userStorageService.js');
 
-var app = angular.module('listsServiceApp', ['idGeneratorServiceApp']);
+var app = angular.module('listsServiceApp', ['idGeneratorServiceApp', 'userStorageServiceApp']);
 
-app.service('listsService', ['$http', 'idGeneratorService', function($http, idGeneratorService) {
+app.service('listsService', ['$http', 'idGeneratorService', 'userStorageService',
+function($http, idGeneratorService, userStorageService) {
   return {
     createList: function(title, user) {
       var id = idGeneratorService.getListId(title, user);
       $http.post('/createList', {title: title, id: id}).then(function(res) {
-
+        userStorageService.addList(id, title);
       });
       return id;
     },
     deleteList: function(list) {
       $http.post('/deleteList', list).then(function(res) {
-        
+        userStorageService.deleteList(list.id);
       });
     }
   };
--- app/share/services/userStorageService.js
+++ app/share/services/userStorageService.js
@@ -3,14 +3,31 @@ require('../../../node_modules/angular-local-storage/dist/angular-local-storage.
 var app = angular.module('userStorageServiceApp', ['LocalStorageModule']);
 
 app.service('userStorageService', ['$http', 'localStorageService', function($http, localStorageService) {
-  var user = {};
-
+  
   this.set = function(newUser) {
     localStorageService.set('user', newUser);
   };
 
   this.get = function() {
     return localStorageService.get('user');
+ };
+
+ this.addList = function(id, title) {
+   user = localStorageService.get('user');
+   if(user) {
+     user.lists.push({title: title, id: id, tasks: []});
+   }
+   localStorageService.set('user', user);
+ };
+
+ this.deleteList = function(id) {
+   user = localStorageService.get('user');
+   if(user) {
+     user.lists.splice(user.lists.map(function(list) {
+       return list.id;
+     }).indexOf(id), 1);
+     localStorageService.set('user', user);
+   }
  };
 
 }]);
