84e50891e805af057a2728e160efd73808030a87
Andrey Sitsko <andrey.sitsko@gmail.com>
2016-02-05 23:46:22 +0300

    config creating/deleting lists in listsService

--- app/share/components/main/lists/listsService.js
+++ app/share/components/main/lists/listsService.js
@@ -1,21 +1,27 @@
 require('../../../services/idGeneratorService.js');
-require('../../../services/userStorageService.js');
+require('../../../services/currentUserService.js');
 
-var app = angular.module('listsServiceApp', ['idGeneratorServiceApp', 'userStorageServiceApp']);
+var app = angular.module('listsServiceApp', ['idGeneratorServiceApp', 'currentUserServiceApp']);
 
-app.service('listsService', ['$http', 'idGeneratorService', 'userStorageService',
-function($http, idGeneratorService, userStorageService) {
+app.service('listsService', ['$http', 'idGeneratorService', 'currentUserService',
+function($http, idGeneratorService, currentUserService) {
   return {
     createList: function(title, user) {
       var id = idGeneratorService.getListId(title, user);
-      $http.post('/createList', {title: title, id: id}).then(function(res) {
-        userStorageService.addList(id, title);
+      currentUserService.addList(id, title);
+      $http.post('/createList', {title: title, id: id}).then(function(res, err) {
+        if(err) {
+          throw err;
+        }
       });
       return id;
     },
     deleteList: function(list) {
-      $http.post('/deleteList', list).then(function(res) {
-        userStorageService.deleteList(list.id);
+      currentUserService.deleteList(list.id);
+      $http.post('/deleteList', list).then(function(res, err) {
+        if(err) {
+          throw err;
+        }
       });
     }
   };
