25b9a92fd8e98f61bb330580b849ffe23ddc41b0
vladislav-averin <exo-1996@tut.by>
2016-03-15 18:24:29 +0300

    finish signUp window markuping

--- app/cores/loginApp.js
+++ app/cores/loginApp.js
@@ -2,9 +2,8 @@ require('../../node_modules/angular/angular.min.js');
 require('../../node_modules/bootstrap/dist/js/bootstrap.min.js');
 require('../share/components/login/signUp/signUpApp.js');
 require('../share/components/login/signIn/signInApp.js');
-require('../share/components/common/header/headerApp.js');
 
-var app = angular.module('loginApp', ['signInApp', 'signUpApp', 'headerApp']);
+var app = angular.module('loginApp', ['signInApp', 'signUpApp']);
 
 app.controller('styleController', ['$scope', function($scope) {
   $scope.stylesheets = [
@@ -12,7 +11,6 @@ app.controller('styleController', ['$scope', function($scope) {
     '../share/components/login/signUp/signUpStyle.css',
     '../share/components/login/signIn/signInStyle.css',
     '../share/components/login/signIn/fonts.css',
-    '../share/components/common/header/headerStyle.css',
     '../share/assets/stylesheets/loginStyle.css',
     '../../node_modules/bootstrap/dist/css/bootstrap.min.css',
     '../../node_modules/bootstrap-social/bootstrap-social.css',
--- server/config/serverPathConfig.js
+++ server/config/serverPathConfig.js
@@ -7,7 +7,6 @@ function config(app) {
   app.use('/assets', express.static('./app/share/assets'));
   app.use('/node_modules', express.static('./node_modules'));
 
-  app.use('/headerView.html', express.static('./app/share/components/common/header/headerView.html'));
   app.use('/signInView.html', express.static('./app/share/components/login/signIn/signInView.html'));
   app.use('/signUpView.html', express.static('./app/share/components/login/signUp/signUpView.html'));
   app.use('/userInfoView.html', express.static('./app/share/components/main/userInfo/userInfoView.html'));
