9f15e01859deebef2abc96164a999c920e02b728
Andrey Sitsko <Andrey_Sitsko@epam.com>
2016-04-04 15:08:07 +0300

    move login middleware to routes

--- server/middleware/login/signOut.js
+++
@@ -1,7 +0,0 @@
-var User = require('../../models/userModel.js');
-
-module.exports = function(req, res) {
-  req.session.destroy();
-  req.logout();
-  res.redirect('/');
-};
--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -1,28 +1,100 @@
 var User = require('../models/userModel.js'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    localSignIn = require('../middleware/login/signIn').local,
-    facebookSignIn = require('../middleware/login/signIn').facebook,
-    facebookSignInCallback = require('../middleware/login/signIn').facebookCallback,
-    signUp = require('../middleware/login/signUp'),
-    signOut = require('../middleware/login/signOut'),
-    authCheck = require('../middleware/login/authCheck.js');
+    passport = require('passport');
 
-function loginRoutes(app) {
+module.exports = function(app) {
 
-  app.get('/logout', signOut);
-  app.post('/signUp', signUp);
-  app.post('/signIn', localSignIn);
+  app.get('/logout', function(req, res) {
+    req.session.destroy();
+    req.logout();
+    res.redirect('/');
+  });
 
-  app.get('/signIn/facebook', facebookSignIn);
-  app.get('/signIn/facebook/callback', facebookSignInCallback);
+  app.post('/signUp', function(req, res) {
+    var newUser = new User({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email
+    });
+    User.find({email: newUser.email}, function(err, user) {
+      if(err) {
+        throw err;
+      }
+      if(user.length) {
+        res.send({success: false, status: 'already exist'});
+      } else {
+        newUser.save(function(err) {
+          if (err) {
+            throw err;
+          }
+        });
+        passport.authenticate('local', function(err, user) {
+          if (err) {
+            return next(err);
+          }
+          if (!user) {
+            res.send({success: false, status: 'auth error'});
+          } else {
+            req.login(user, function(err) {
+              if (err) {
+                throw err;
+              }
+            });
+            res.send({success: true, user: user});
+          }
+        })(req, res);
+      }
+    });
+  });
 
-  app.get('/', authCheck.login);
-  app.get('/main', authCheck.main);
+  app.post('/signIn', function(req, res, next) {
+    passport.authenticate('local', function(err, user) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        res.send({success: false});
+      } else {
+        req.login(user, function(err) {
+          if (err) {
+            throw err;
+          }
+        });
+        res.send({success: true, user: user});
+      }
+    })(req, res);
+  });
+
+  app.get('/signIn/facebook', function(req, res, next) {
+    passport.authenticate('facebook');
+  });
+
+  app.get('/signIn/facebook/callback', function(req, res, next) {
+    passport.authenticate('facebook', {
+      successRedirect : '/main',
+      failureRedirect : '/'
+    });
+  });
+
+  app.get('/', function (req, res) {
+    if (req.user) {
+        res.redirect('/main');
+    } else {
+        res.render('./loginView.html');
+    }
+  });
+
+  app.get('/main', function (req, res) {
+      if (req.user) {
+          res.render('./mainView.html');
+      } else {
+          res.redirect('/');
+      }
+  });
+
   app.get('/currentUser', function(req, res) {
     res.send(req.user);
   });
-  
+
   app.get('/api/users', function(req, res) {
     User.find({}, function(err, users) {
       res.send(users);
@@ -33,6 +105,4 @@ function loginRoutes(app) {
   });
 
   return app;
-}
-
-module.exports = loginRoutes;
+};
