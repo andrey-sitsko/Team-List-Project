dfdbb18a4f26c4fa0397265ba6e64875b8c8a5fc
Andrey Sitsko <Andrey_Sitsko@epam.com>
2016-04-05 09:39:07 +0300

    config generating different id at same milisecond

--- app/share/services/idGeneratorService.js
+++ app/share/services/idGeneratorService.js
@@ -3,15 +3,15 @@ var app = angular.module('idGeneratorServiceApp', []);
 app.service('idGeneratorService', function() {
   return {
     getListId: function(title, user) {
-      id = 'L' + '_' + user.email + '_' + title + '_' + getMiliseconds();
+      id = 'L' + '_' + user.email + '_' + title + '_' + getRandomIdPart();
       return id;
     },
     getTaskId: function(title, user) {
-      id = 'T' + '_' + user.email + '_' + title + '_' + getMiliseconds();
+      id = 'T' + '_' + user.email + '_' + title + '_' + getRandomIdPart();
       return id;
     },
     getSubTaskId: function(title, user) {
-      id = 'ST' + '_' + user.email + '_' + title + '_' + getMiliseconds();
+      id = 'ST' + '_' + user.email + '_' + title + '_' + getRandomIdPart();
       return id;
     }
   };
@@ -19,4 +19,10 @@ app.service('idGeneratorService', function() {
 
 function getMiliseconds() {
   return (new Date()).valueOf().toString();
+}
+
+function getRandomIdPart() {
+  var max = 1000,
+      min = 0;
+  return (getMiliseconds() + Math.random() * (max - min + 1) + min);
 }
