bf8f0900d66b7b324347e24142dfe9dc6039e4d5
Andrey Sitsko <Andrey_Sitsko@epam.com>
2016-04-09 11:52:03 +0300

    add google auth strategy

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -1,5 +1,6 @@
 var localStrategy = require('./passportStrategies/localStrategy.js'),
     facebookStrategy = require('./passportStrategies/facebookStrategy.js'),
+    googleStrategy = require('./passportStrategies/googleStrategy.js'),
     User = require('../models/userModel'),
     passport = require('passport');
 
@@ -7,6 +8,7 @@ module.exports = function() {
 
   passport.use(localStrategy);
   passport.use(facebookStrategy);
+  passport.use(googleStrategy);
 
   passport.serializeUser(function(user, done) {
     done(null, user);
---
+++ server/config/passportStrategies/googleStrategy.js
@@ -0,0 +1,12 @@
+var googleStrategy = require('passport-google-plus');
+
+module.exports = new googleStrategy({
+    clientId: '706642257689-oirpr00cf06hjdl68tavfn4t86t45aq7.apps.googleusercontent.com',
+    clientSecret: 't1b7hi7fBroPHAmqdF6nBybd',
+    callbackURL: 'http://localhost:8000/signIn/google/callback'
+  },
+  function(tokens, profile, done) {
+    console.log('google strategy');
+    done(null, profile, tokens);
+  }
+));
